1548747728ab8daa0d1c74e99289fa1801e4f4d6
Yonathan Tesfaye <yt040448@gmail.com>
2023-06-30 12:48:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def what(message, chat_id, message_id):
     bot.send_chat_action(chat_id, "typing")
     for chunk in client.send_message("capybara", message, with_chat_break=False):
         response += chunk["text_new"]
-        if (!bot.edit_message_text(response, chat_id,
-                                  message_id, parse_mode="Markdown")):
+        if (bot.edit_message_text(response, chat_id,
+                                  message_id, parse_mode="Markdown") == False):
             miss = True
     if (miss):
         bot.send_message(chat_id, response, parse_mode="Markdown")
