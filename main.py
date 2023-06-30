94d4297ef479616f9fa8f69655ae2af8bd3518d7
Yonathan Tesfaye <yt040448@gmail.com>
2023-06-30 10:29:33 +0000

    Update 2 files
    Update main.py
    Update requirements.txt

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import poe
+import telebot
 
-client = poe.Client("7oifB7lowK8lxUTL-TnVpw%3D%3D")
-message = "What is your name?"
+bot = telebot.TeleBot("6313102560:AAHcAM7TRGVX2aNmpfjLaeWWOMHSfGm9E6s", parse_mode=None)
 
-response = ""
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
 
-for chunk in client.send_message("capybara", message, with_chat_break=True):
-    response += chunk["text_new"]
-print(response)
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
+
+def what():
+    client = poe.Client("7oifB7lowK8lxUTL-TnVpw%3D%3D")
+    message = "What is your name?"
+
+    response = ""
+
+    for chunk in client.send_message("capybara", message, with_chat_break=True):
+        response += chunk["text_new"]
+    print(response)
