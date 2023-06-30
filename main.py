38a7f2025c4712c5d4dc8ce83e417ec407392e91
Yonathan Tesfaye <yt040448@gmail.com>
2023-06-30 11:06:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-# import asyncio
 import poe
 import telebot
+
+def what(message):
+    client = poe.Client("7oifB7lowK8lxUTL-TnVpw%3D%3D")
+    response = ""
+    for chunk in client.send_message("capybara", message, with_chat_break=True):
+        response += chunk["text_new"]
+    return response
 
 bot = telebot.TeleBot("6313102560:AAHcAM7TRGVX2aNmpfjLaeWWOMHSfGm9E6s", parse_mode=None)
 
@@ -11,17 +17,5 @@ def send_welcome(message):
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
     bot.reply_to(message, what(message.text))
-    # asyncio.run(respond)
 
 bot.infinity_polling()
-
-# async def respond(msg):
-#     await what()
-#     bot.reply_to(msg, msg.text)
-
-async def what(message):
-    client = poe.Client("7oifB7lowK8lxUTL-TnVpw%3D%3D")
-    response = ""
-    for chunk in await client.send_message("capybara", message, with_chat_break=True):
-        response += chunk["text_new"]
-    return response
