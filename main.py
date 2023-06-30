f44d21acf33271a5b13456e13584c44269f0b6f6
Yonathan Tesfaye <yt040448@gmail.com>
2023-06-30 11:46:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import poe
 import telebot
 
+
 def what(message):
     client = poe.Client("7oifB7lowK8lxUTL-TnVpw%3D%3D")
     response = ""
@@ -8,15 +9,21 @@ def what(message):
         response += chunk["text_new"]
     return response
 
-bot = telebot.TeleBot("6313102560:AAHcAM7TRGVX2aNmpfjLaeWWOMHSfGm9E6s", parse_mode=None)
+
+bot = telebot.TeleBot(
+    "6313102560:AAHcAM7TRGVX2aNmpfjLaeWWOMHSfGm9E6s", parse_mode="Markdown")
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Try Free AI robot for free")
+
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    bot.reply_to(message, "Please wait...")
+    sent = bot.reply_to(message, "Please wait...")
+    sent.text = "Wait..."
     bot.reply_to(message, what(message.text))
+
 
 bot.infinity_polling()
